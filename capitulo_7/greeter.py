db325bd1caa84db2aaae9f1cdf78ea4d3fe79926
andreregor <andreregor@gmail.com>
2021-02-18 22:20:56 -0300

    Alterado a aplicação para exemplificar um prompt com mais de uma linha

--- capitulo_7/greeter.py
+++ capitulo_7/greeter.py
@@ -1,4 +1,8 @@
 # Escrevendo prompts claros
 
-name = input("Please enter your name: ")
-print('Hello, ' + name + '!')
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+
+print("\nHello, " + name + "!")
